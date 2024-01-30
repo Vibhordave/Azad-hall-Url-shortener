bc1d065415d6ad86e1ad51fb4e0fb47052d4230e
Shivam Kumar <shivamkmishra09@kgpian.iitkgp.ac.in>
2024-01-31 01:10:13 +0530

    dashboard created, icons added, register button position fixed

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Dashboard from '../components/DashBoard';
+import { BsToggles } from "react-icons/bs";
+import { FaToggleOn } from "react-icons/fa";
+import { FaToggleOff } from "react-icons/fa6";
 const HomePage = () => {
   const [url, setUrl] = useState('');
+  const [autoPaste, setAutoPaste] = useState(false); 
+  const [linkCount, setLinkCount] = useState(0); // Add this line
 
   const shortenUrl = () => {
+    if (linkCount >= 5) {
+      alert('You have reached the maximum number of links. Please register to create more links.');
+      return;
+    }
+
     // Implement URL shortening logic here
     console.log('URL Shortened');
-  };
 
+    setLinkCount(linkCount + 1); // Add this line
+  };
+  const toggleAutoPaste = () => { // Add this function
+    setAutoPaste(!autoPaste);
+  };
   return (
     <div className="home-container">
         <div className='home-item-box'>
@@ -24,8 +38,28 @@ const HomePage = () => {
         
         <button type='submit' onClick={shortenUrl}>Shorten Now!</button>
         </div>
-        <p className="auto-paste">Auto Paste from Clipboard</p>
-            <p>You can create 05 more links. Register now to enjoy Unlimited Usage</p>
+        <div>
+
+        <p className="auto-paste">
+        <button 
+          onClick={toggleAutoPaste} 
+          style={{ 
+            marginRight: '0px', 
+            backgroundColor: '#0B101B', 
+            display: 'flex', 
+            alignItems: 'center', 
+            justifyContent: 'center' 
+          }}
+        >
+          {autoPaste ? 
+            <FaToggleOn style={{ color: 'green', fontSize: '30px',marginRight: '10px'  }} /> : 
+            <FaToggleOff style={{ color: 'red', fontSize: '30px', marginRight: '10px'  }} />
+          }
+          Auto Paste from Clipboard
+        </button>
+      </p>
+    </div>
+    <p>You can create <span style={{ color: 'skyblue', fontSize:'1.1rem', fontWeight:'30' }}>{5 - linkCount}</span>  more links. Register now to enjoy Unlimited Usage</p>
          
         {/* Add more features like copy to clipboard etc. */}
       </div>
@@ -37,8 +71,9 @@ const HomePage = () => {
             <button className="view">View</button>
           </div>
         </div>
-        
-        <p className="home-bottom"><Link to="/register"className="register-link">Register Now</Link> to enjoy Unlimited History</p>      
+        <Dashboard />
+
+        <p className="home-bottom"><Link to="/subscription"className="register-link">Register Now</Link> to enjoy Unlimited History</p>      
       
     </div>
   );
